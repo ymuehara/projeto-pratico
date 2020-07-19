f8bd099a7a7edd9d4363f49055b79f83f0d8a302
Yuri Uehara <yuriuehara@hotmail.com>
2020-07-19 17:34:08 -0300

    correcao middleware whitenoise

--- central_de_erros/settings_prod.py
+++ central_de_erros/settings_prod.py
@@ -8,6 +8,5 @@ DATABASES = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
