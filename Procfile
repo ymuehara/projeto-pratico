cd717341a27d413ccadcead3cc5cd3a05d8eea63
Yuri Uehara <yuriuehara@hotmail.com>
2020-07-19 16:23:30 -0300

    adicao procfile e configuracoes para producao

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn projeto-pratico.wsgi --max-requests 1200
---
+++ central_de_erros/settings_prod.py
@@ -0,0 +1,13 @@
+from .settings import *
+import dj_dabatase_url
+DEBUG = False
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
